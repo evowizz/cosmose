8452fcc7518278e26150bae0fa3e8fd805902afb
Dylan Roussel <mail@evowizz.dev>
2024-08-18 15:45:07 +0200

    Bump version to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "dev.evowizz.cosmose"
         minSdk = 31
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.1"
     }
 
     buildTypes {
